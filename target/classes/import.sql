6dd5705b0f278aa0c8830caf120143c01a79f161
jasrzhang <zhangshurui1989@gmail.com>
2023-01-30 20:10:44 +1030

    configured basic structure and H2 Database

---
+++ target/classes/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO Dog VALUES (1,'Didi','Miniature Schnauzer','German');
+INSERT INTO Dog VALUES (2,'Meimei','Jack Russell','England');
+INSERT INTO Dog (id, name, breed, origin) VALUES (3,'Brownie','Pug','China');
+INSERT INTO Dog (id, name, breed, origin) VALUES (4,'Luffy','French Bulldog','France');
+INSERT INTO Dog (id, name, breed, origin) VALUES (5,'Buddy','Labradoodle','Australia');
